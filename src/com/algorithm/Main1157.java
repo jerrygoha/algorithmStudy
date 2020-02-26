f349fa81536b2f14cc877f166d833f2dbba557b2
jehunha <jerrygoha@naver.com>
2020-02-26 14:23:34 +0900

    2020-02-26

--- src/com/algorithm/Main1157.java
+++ src/com/algorithm/Main1157.java
@@ -14,6 +14,66 @@ class solve1157{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         String inputText = br.readLine();
+        String[] alpha = inputText.split("");
+
+        for(String alp : alpha){
+
+            if(alp.equalsIgnoreCase("a")){
+
+            }else if(alp.equalsIgnoreCase("b")){
+
+            }else if(alp.equalsIgnoreCase("c")){
+
+            }else if(alp.equalsIgnoreCase("d")){
+
+            }else if(alp.equalsIgnoreCase("e")){
+
+            }else if(alp.equalsIgnoreCase("f")){
+
+            }else if(alp.equalsIgnoreCase("g")){
+
+            }else if(alp.equalsIgnoreCase("h")){
+
+            }else if(alp.equalsIgnoreCase("i")){
+
+            }else if(alp.equalsIgnoreCase("j")){
+
+            }else if(alp.equalsIgnoreCase("k")){
+
+            }else if(alp.equalsIgnoreCase("l")){
+
+            }else if(alp.equalsIgnoreCase("m")){
+
+            }else if(alp.equalsIgnoreCase("n")){
+
+            }else if(alp.equalsIgnoreCase("o")){
+
+            }else if(alp.equalsIgnoreCase("p")){
+
+            }else if(alp.equalsIgnoreCase("q")){
+
+            }else if(alp.equalsIgnoreCase("r")){
+
+            }else if(alp.equalsIgnoreCase("s")){
+
+            }else if(alp.equalsIgnoreCase("t")){
+
+            }else if(alp.equalsIgnoreCase("u")){
+
+            }else if(alp.equalsIgnoreCase("v")){
+
+            }else if(alp.equalsIgnoreCase("w")){
+
+            }else if(alp.equalsIgnoreCase("x")){
+
+            }else if(alp.equalsIgnoreCase("y")){
+
+            }else if(alp.equalsIgnoreCase("z")){
+
+            }
+
+        }
+
 
     }
 }
