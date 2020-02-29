f6ec1613aa5bbf1ae27cec149f26a5f821f10c78
jehunha <jerrygoha@naver.com>
2020-03-01 00:47:41 +0900

    2020-03-01

---
+++ src/com/algorithm/Main1193.java
@@ -0,0 +1,26 @@
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main1193 {
+    public static void main(String[] args) throws IOException {
+        new solve1193().problem();
+    }
+}
+class solve1193{
+    public void problem() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        int numberArr = 0;
+        int[] numerator = new int[10000000];
+        int[] denominator = new int[10000000];
+
+        while
+
+
+
+    }
+}
