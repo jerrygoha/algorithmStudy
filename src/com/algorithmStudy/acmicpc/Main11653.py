ebc6a4f42f59ac0ac3bd8970b1c059ae54f2f622
Earth_Science_Teacher <jerrygoha@naver.com>
2021-07-16 23:51:04 +0900

    2021.07.16 boj

--- src/com/algorithmStudy/acmicpc/Main11653.py
+++ src/com/algorithmStudy/acmicpc/Main11653.py
@@ -1,19 +1,20 @@
 import math
 def solve(x):
-    #에라토스테네스의 체를 사용해서 소수 리스트 생성
-    tempList = list(range(2, x))
+    # #에라토스테네스의 체를 사용해서 소수 리스트 생성
+    #시간초과때문에 해당 로직은 제외하고 그냥 전부다 돌리기로...
+    # tempList = list(range(2, x))
+    #
+    # for i in range(2, math.ceil(math.sqrt(x))):
+    #     for temp in tempList:
+    #         if temp/i==1:
+    #             pass
+    #         elif temp%i==0:
+    #             tempList.remove(temp)
 
-    for i in range(2, math.ceil(math.sqrt(x))):
-        for temp in tempList:
-            if temp/i==1:
-                pass
-            elif temp%i==0:
-                tempList.remove(temp)
-
-    #만든 소수 리스트로 입력받은 수를 나눠보자!
+    #만든 소수 리스트로 입력받은 수를 나눠보자! -> 위의 이유로 소수 리스트가 아닌 전부다!
     dev = x
     while dev!=1:
-        for value in tempList:
+        for value in range(2, x+1):
             if dev % value == 0:
                 print(value)
                 dev = dev // value
