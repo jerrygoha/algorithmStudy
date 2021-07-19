6fd70e675746b21d6428385c3a0d03d9ccfee9a9
Earth_Science_Teacher <jerrygoha@naver.com>
2021-07-20 04:45:52 +0900

    2021.07.20 boj

--- src/com/algorithmStudy/acmicpc/Main1929.py
+++ src/com/algorithmStudy/acmicpc/Main1929.py
@@ -7,8 +7,7 @@ def solve(m, n):
     #에라토스테네스의 체 에서는 2부터 n의 제곱근까지의 수를 확인하기때문에 n의 제곱근까지 리스트 하나 만들어줌
     sqrtList = list(range(2, math.ceil(math.sqrt(n))+1))
 
-    #에라토스테네스의 체
-    #2부터 n의 제곱근까지의 모든 수를 확인하며, i가 소수인 경우 i를 제외한 모든 배수를 지우도록 한다.
+
     for i in sqrtList:
         for value in resultList:
             if value//i==1:
@@ -27,6 +26,8 @@ def otherSolve(m, n):
     resultList[0] = False
     resultList[1] = False
 
+    # 에라토스테네스의 체
+    # 2부터 n의 제곱근까지의 모든 수를 확인하며, i가 소수인 경우 i를 제외한 모든 배수를 지우도록 한다.
     for i in range(2, math.ceil(math.sqrt(n))+1):
         if resultList[i]:
             #i를 제외한 모든 i의 배수 지우기
