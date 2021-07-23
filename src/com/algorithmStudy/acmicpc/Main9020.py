b499f04a392aa1bd7e8c2ceebbf0ed1e9ba369de
Earth_Science_Teacher <jerrygoha@naver.com>
2021-07-23 17:22:43 +0900

    2021.07.23 boj

--- src/com/algorithmStudy/acmicpc/Main9020.py
+++ src/com/algorithmStudy/acmicpc/Main9020.py
@@ -1,6 +1,7 @@
 import math
 class GoldBah:
 
+    #생성자
     def __init__(self):
         #소수 테이블 만들어놓기
         self.minorityList = [True for _ in range(10000)]
@@ -17,14 +18,33 @@ class GoldBah:
     def solve(self, n):
         #소수 테이블 차례대로 훓으면서..?
         #짝수에서 첫번째 소수를 뺐을때의 결과가 소수여야한다!
+        firstNum = n
+        leftNum = 0
+        rightNum = 0
+        for index, value in enumerate(self.minorityList):
+            if value==False:
+                pass
+            #소수 빼기
+            elif value==True:
+                if index>(n//2):
+                    return "{} {}".format(leftNum, firstNum-leftNum)
+                rightNum = firstNum-index
+                if self.minorityList[rightNum]==False:
+                    pass
+                #뺀값이 소수
+                elif self.minorityList[rightNum]==True:
+                    leftNum = index
+                    pass
 
 
-        print(self.minorityList)
-        return 0
 
 if __name__ == "__main__" :
-    caseNumber = int(input())
-    # for caseCnt in range(caseNumber):
-    #     n = int(input())
     a = GoldBah()
-    a.solve(caseNumber)
+    caseNumber = int(input())
+    resultList = []
+    for caseCnt in range(caseNumber):
+        n = int(input())
+        resultList.append(a.solve(n))
+
+    print(*resultList, sep="\n")
+
