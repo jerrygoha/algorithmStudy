7016954ac52de1b7f5b18fb581bb801d67f7f31d
Earth_Science_Teacher <jerrygoha@naver.com>
2021-07-25 04:29:39 +0900

    2021.07.25 boj (ing..)

---
+++ src/com/algorithmStudy/acmicpc/Main1085.py
@@ -0,0 +1,13 @@
+import math
+class Escape:
+    # (x, y) : 현위치
+    # 왼 아래 꼭짓점 (0, 0), 오른 위 꼭짓점 (w, h)
+    def solve(self, x, y, w, h):
+
+
+
+
+if __name__ == "__main__":
+    a = Escape()
+    x, y, w, h = map(int, input().split())
+    a.solve(x, y, w, h)
