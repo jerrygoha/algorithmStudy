85f2810eefcfef1990973f35f15140afd6927607
Earth_Science_Teacher <jerrygoha@naver.com>
2021-08-04 11:44:04 +0900

    2021.08.04 boj

---
+++ src/com/algorithmStudy/acmicpc/Main2447.py
@@ -0,0 +1,15 @@
+class MakeStar :
+    def makeStar(self, n):
+        #패턴 최소단위
+
+        minPattern = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+        #추후풀이
+
+
+        return 0
+
+
+if __name__ == "__main__":
+    input = int(input())
+    a = MakeStar()
+    a.makeStar(input)
