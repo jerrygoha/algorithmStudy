c655a8e59c0480eae9878c43a3149b38253f5555
Earth_Science_Teacher <jerrygoha@naver.com>
2021-08-08 02:04:21 +0900

    2021.08.08 boj

--- src/com/algorithmStudy/acmicpc/Main2798.py
+++ src/com/algorithmStudy/acmicpc/Main2798.py
@@ -1,10 +1,10 @@
 import itertools
 
 class Blackjack :
-    def blackjack(self, n, m, cardList) :
+    def blackjackItertools(self, n, m, cardList) :
 
         #m에 최대한 가까운 카드 3장의 합
-        max = 0
+        maxSum = 0
 
         #조합 사용하여 모든 경우 구하고 그중 조건 만족하는 제일 큰 경우
         resultList = list(itertools.permutations(cardList, 3))
@@ -12,11 +12,28 @@ class Blackjack :
         #각 경우의 수(튜플)의 합계를 구하면서 max값 갱신
         for tuple in resultList :
             newTuple = list(tuple)
-            if sum(newTuple)>max and sum(newTuple)<=m :
-                max = sum(newTuple)
-            else:
-                pass
-        print(max)
+            if sum(newTuple)>m :
+                continue
+            else :
+                maxSum = max(maxSum, sum(newTuple))
+
+        print(maxSum)
+
+    def blackjackBruteForce(self, n, m, cardList):
+
+        #m에 가까운 최대값
+        maxSum = 0
+
+        #Brute Force
+        for firstPickIndex in range(n):
+            for secondPickIndex in range(firstPickIndex+1, n):
+                for thirdPickIndex in range(secondPickIndex+1, n):
+                    if cardList[firstPickIndex] + cardList[secondPickIndex] + cardList[thirdPickIndex] > m:
+                        continue
+                    else:
+                        maxSum = max(maxSum, cardList[firstPickIndex]+cardList[secondPickIndex]+cardList[thirdPickIndex])
+
+        print(maxSum)
 
 
 if __name__ == "__main__" :
@@ -24,4 +41,7 @@ if __name__ == "__main__" :
     #input
     n, m = map(int, input().split())
     cardList = list(map(int, input().split()))
-    case1.blackjack(n, m, cardList)
+    print("-----Using Itertools-----")
+    case1.blackjackItertools(n, m, cardList)
+    print("-----Using Brute Force-----")
+    case1.blackjackBruteForce(n, m, cardList)
