972170050b0a9e066af0a023bbe63b36f059c729
Earth_Science_Teacher <jerrygoha@naver.com>
2021-08-17 01:38:26 +0900

    2021.08.17 boj

---
+++ src/com/algorithmStudy/acmicpc/Main7568.py
@@ -0,0 +1,17 @@
+class Dongchi :
+    def solution(self, inputList):
+        #각 사람마다 랭킹 저장하는 리스트
+        rankList = []
+
+
+        return None
+
+
+if __name__ == "__main__" :
+    a = Dongchi()
+    inputList = []
+    n = int(input())
+    for _ in range(n):
+        inputList.append(tuple(map(int, input().split())))
+
+    a.solution(inputList)
